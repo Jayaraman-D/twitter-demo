d2c19fa63ec1ce678d028721948401bb4b60b7e1
Jayaraman-D <mr.jairam.d@gmail.com>
2025-10-16 06:43:46 +0530

    home components was build

--- frontend/twitter/Components/PostsComponent/Posts.jsx
+++ frontend/twitter/Components/PostsComponent/Posts.jsx
@@ -7,20 +7,30 @@ const Posts = () => {
   const [feedtype, setFeedtype] = useState('foryou');
 
   const showForYou = () => {
-    console.log(`For You button was clicked`);
     setFeedtype('foryou');
   };
 
   const showFollowing = () => {
-    console.log('Following posts button was clicked')
     setFeedtype('following');
   };
 
   return (
     <div className="posts">
       <div className="feedtype">
-        <button type="button" onClick={showForYou}>For You</button>
-        <button type="button" onClick={showFollowing}>Following</button>
+        <button
+          type="button"
+          onClick={showForYou}
+          className={feedtype === 'foryou' ? 'active' : ''}
+        >
+          For You
+        </button>
+        <button
+          type="button"
+          onClick={showFollowing}
+          className={feedtype === 'following' ? 'active' : ''}
+        >
+          Following
+        </button>
       </div>
 
       {feedtype === 'foryou' ? <Post /> : <Following />}
--- frontend/twitter/src/pages/Home/Homepage/Homepage.jsx
+++ frontend/twitter/src/pages/Home/Homepage/Homepage.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import './Homepage.css'
-import Sidebar from '../../../../Components/sidebarComponent/Sidebar'
-import Posts from '../../../../Components/PostsComponent/Posts'
-import Suggestions from '../../../../Components/SuggestionsComponent/Suggestions'
+import React from 'react';
+import './Homepage.css';
+import Sidebar from '../../../../Components/sidebarComponent/Sidebar';
+import Posts from '../../../../Components/PostsComponent/Posts';
+import Suggestions from '../../../../Components/SuggestionsComponent/Suggestions';
 
 const Homepage = () => {
   return (
-    <div className='homepage'>
-        <Sidebar/>
-        <Posts/>
-        <Suggestions/>
+    <div className='homepage-container'>
+      <Sidebar />
+      <main className='main-content'>
+        <Posts />
+      </main>
+      <aside className='suggestions-container'>
+        <Suggestions />
+      </aside>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
