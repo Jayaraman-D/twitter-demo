695c97ef3ad1963c739ab07bd8b2d0b9b70c1fe8
Jayaraman-D <mr.jairam.d@gmail.com>
2025-10-16 20:11:33 +0530

    new post column is added

--- frontend/twitter/Components/PostsComponent/Post.jsx
+++ frontend/twitter/Components/PostsComponent/Post.jsx
@@ -3,36 +3,55 @@ import './Post.css'
 import postimg from '/logos/x-logo.png'
 
 const Post = () => {
-  return (
-    <div className='post'>
-        <div className="posted">
-            <h3>This is a post 1</h3>
-            <div className="buttons">
-                <button>like</button>
-                 <button>Comment</button>
-                  <button>share</button>
-            </div>
-        </div>
+    return (
+        <div className='post'>
+            <div className="new-post">
+                <div className="text-area">
+                    <div className="user-info">
+                        <img src={postimg} alt="dp" />
+                        <h3 className="username">ram</h3>
+                    </div>
+                    <input type="text" placeholder="What is happening?" />
+                </div>
 
-        <div className="posted">
-            <h3>This is a post 2</h3>
-            <div className="buttons">
-                <button>like</button>
-                 <button>Comment</button>
-                  <button>share</button>
+                <div className="post-btn">
+                    <div className="icons">
+                        <i className="bi bi-image"></i>
+                        <i className="bi bi-emoji-smile"></i>
+                    </div>
+                    <button type="button">Post</button>
+                </div>
             </div>
-        </div>
 
-        <div className="posted">
-            <h3>This is a post 4</h3>
-            <div className="buttons">
-                <button>like</button>
-                 <button>Comment</button>
-                  <button>share</button>
+
+            <div className="posted">
+                <h3>This is a post 1</h3>
+                <div className="buttons">
+                    <button>like</button>
+                    <button>Comment</button>
+                    <button>share</button>
+                </div>
+            </div>
+
+            <div className="posted">
+                <h3>This is a post 2</h3>
+                <div className="buttons">
+                    <button>like</button>
+                    <button>Comment</button>
+                    <button>share</button>
+                </div>
+            </div>
+
+            <div className="posted">
+                <h3>This is a post 4</h3>
+                <div className="buttons">
+                    <button>like</button>
+                    <button>Comment</button>
+                    <button>share</button>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Post
