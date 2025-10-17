6c3e936cc9c9b25e0d6a59e0d2cf35420a80af80
Jayaraman-D <mr.jairam.d@gmail.com>
2025-10-17 23:51:46 +0530

    suggestion component is build

--- frontend/twitter/Components/SuggestionsComponent/Suggestions.jsx
+++ frontend/twitter/Components/SuggestionsComponent/Suggestions.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Suggestions.css'
 import dp from '/logos/x-logo.png'
+import axios from 'axios';
+import { BaseURL } from '../../BaseUrl/BaseURL';
 
 const Suggestions = () => {
+  const [suggestedUsers, setSuggestedUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get(`${BaseURL}/api/user/suggestions`, { withCredentials: true });
+        setSuggestedUsers(res.data);
+      } catch (error) {
+        console.log(`Error occured in suggested fetch user: ${error.message}`);
+      }
+    }
+    fetchUser();
+  }, [])
+
   return (
-    <div className='suggestions'>
-      <div className="suggestUser">
-        <img src={dp} alt='dp'></img>
-        <p className='userName'>Karthik</p>
-      </div>
-      <div className="suggestUser">
-        <img src={dp} alt='dp'></img>
-        <p className='userName'>Balaji</p>
-      </div>
-      <div className="suggestUser">
-        <img src={dp} alt='dp'></img>
-        <p className='userName'>Senthil Kumar</p>
-      </div>
-    </div>
-  )
+  <div className="suggestions">
+    {suggestedUsers.length === 0 ? (
+      <p>No users</p>
+    ) : (
+      suggestedUsers.map((user) => (
+        <div className="suggestUser" key={user._id}>
+          <div className="user-info">
+            <img src={user.profileImg || dp} alt="dp" />
+            <p className="username">{user.username}</p>
+          </div>
+          <button type="button">Follow</button>
+        </div>
+      ))
+    )}
+  </div>
+);
+
 }
 
 export default Suggestions
